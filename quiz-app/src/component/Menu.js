3b39b277d2ac15b8718fabb48618afeb75967254
thapanankulab25 <s6503052422108@email.kmutnb.ac.th>
2024-02-05 10:54:01 +0700

    ีีupdate score

--- quiz-app/src/component/Menu.js
+++ quiz-app/src/component/Menu.js
@@ -8,8 +8,9 @@ const Menu = ()=>{
     return(
         
         <div className="menu">
-               <img src={"https://kmutnb.ac.th/KMUTNB/media/kmutnb/images/kmutnb_index_Logo.png"} alt="Logo" />
-            <h1>Menu Component</h1>
+               <img src={"https://scontent.fbkk5-5.fna.fbcdn.net/v/t1.6435-9/95399213_1110446755995290_7097007511848353792_n.png?_nc_cat=104&ccb=1-7&_nc_sid=7f8c78&_nc_ohc=jAk-dZcPzagAX9KhNKU&_nc_ht=scontent.fbkk5-5.fna&oh=00_AfDo8ytnMlX9yM2-gdDR89qNE87mTbyc-vCf8rA_S2D3mg&oe=65E7C972"} width="250px" height="250px" alt="Logo" />
+            <h1>แบบทดสอบแฟนพันธุ์แท้เพลง<br></br>พี่เสก Loso</h1>
+           
             <button onClick={()=>setAppState("quiz")}>เริ่มทำแบบทดสอบ</button>
         </div>
     )
--- quiz-app/src/component/Score.js
+++ quiz-app/src/component/Score.js
@@ -9,10 +9,26 @@ const Score = ()=>{
         setScore(0)
         setAppState("menu")
     }
+
+    const getPerformanceLevel = () => {
+        if (score === QuestionsData.length) {
+          return "เยี่ยมมาก";
+        } else if (score >= 4) {
+          return "ดีมาก";
+        } else if (score >= 2) {
+          return "ดี";
+        } else if (score >= 1) {
+          return "พอใช้นะ";
+        } else {
+          return "ไม่ถูกเลยเหรอ";
+        }
+      };
+
     return(
         <div className="score">
             <h1>สรุปผลคะแนน</h1>
             <h2>{score}/{QuestionsData.length}</h2>
+            <p>{getPerformanceLevel()}</p>
             <button onClick={restartApp}>ทำแบบทดสอบอีกครั้ง</button>
         </div>
     )
