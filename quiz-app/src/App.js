0c17b342eb3b26b6cb8db4eb9c1228315940cc8f
thapanankulab25 <s6503052422108@email.kmutnb.ac.th>
2024-02-05 09:50:39 +0700

    add questiondata

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -2,7 +2,7 @@ import {createContext, useState} from "react"
 import Menu from "./component/Menu";
 import Quiz from "./component/Quiz";
 import Score from "./component/Score";
-
+import './App.css'
 export const DataContext = createContext()
 function App() {
   const [appState,setAppState] = useState("menu")
